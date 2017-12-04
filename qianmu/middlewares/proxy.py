767e58d7543fee0d15aba5504f640be08fb3216f
guye <guye999@gmail.com>
2017-12-05 02:07:22 +0800

    auto remove the proxy which has been banned

--- qianmu/middlewares/proxy.py
+++ qianmu/middlewares/proxy.py
@@ -1,13 +1,22 @@
-# -*- coding: utf-8 -*-
+#coding:utf-8
 import random
+import logging
+from urllib.request import _parse_proxy
 from scrapy.exceptions import NotConfigured
 
 
+logger = logging.getLogger()
+
+def reform_proxy(url):
+    proxy_type, user, password, hostport = _parse_proxy(url)
+    return '%s://%s' % (proxy_type, hostport)
+
 class RandomProxyMiddleware(object):
-    """该中间件负责给每个请求随机分配代理IP"""
 
     def __init__(self, settings):
         self.proxies = settings.getlist('PROXIES')
+        self.max_failed = settings.getint('PROXY_MAX_FAILED', 3)
+        self.stats = {}.fromkeys(map(reform_proxy, self.proxies), 0)
 
     def random_proxy(self):
         return random.choice(self.proxies)
@@ -23,3 +32,17 @@ class RandomProxyMiddleware(object):
     def process_request(self, request, spider):
         if 'proxy' not in request.meta:
             request.meta['proxy'] = self.random_proxy()
+
+    def process_response(self, request, response, spider):
+        cur_proxy = request.meta['proxy']
+        if response.status >= 400:
+            self.stats[cur_proxy] += 1
+        if self.stats[cur_proxy] > self.max_failed:
+            for proxy in self.proxies:
+                if cur_proxy.endswith(proxy.split('@')[-1]):
+                    self.proxies.remove(proxy)
+                    break
+            logger.warn('proxy %s remove from proxies list' % cur_proxy)
+        return response
+
+
